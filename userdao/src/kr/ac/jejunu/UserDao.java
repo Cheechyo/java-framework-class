6e0013bd71acbc550ec561e89fea6c03010e7d41
Cheechyo <kwj9211@gmail.com>
2017-04-07 18:43:41 +0900

    context to (Spring) jdbcTemplate
    Delete가 안되는 이유는 Exception 처리가 안되서(spring tx에 exception이 있음)

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -1,38 +1,58 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by Cheechyo on 2017. 3. 15..
  */
 public class UserDao {
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public User get(Long id) throws SQLException, ClassNotFoundException {
         final String sql = "select * from userinfo where id = ?";
         Object[] params = new Object[]{id};
-        return jdbcContext.queryForObject(sql, params);
+        return jdbcTemplate.queryForObject(sql, params, (resultSet, i) -> {
+            User user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+            return user;
+        });
     }
 
     public Long add(User user) throws ClassNotFoundException, SQLException {
         final String sql = "INSERT INTO userinfo(name,password) values (?, ?)";
         Object[] params = new Object[]{user.getName(), user.getPassword()};
-        return jdbcContext.insert(sql, params);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder);
+        return (Long) keyHolder.getKey();
     }
 
     public void update(User user) {
         String sql = "UPDATE userinfo SET NAME = ?, PASSWORD = ? WHERE ID = ?";
         Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Long id) {
         String sql = "DELETE FROM userinfo where id = ?";
         Object[] params = new Object[]{id};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
