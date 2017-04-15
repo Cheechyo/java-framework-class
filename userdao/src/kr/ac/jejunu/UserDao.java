e924eb029068170d793db14c9ebbff3bd04af7dc
Cheechyo <kwj9211@gmail.com>
2017-04-15 14:44:11 +0900

    14. 각자 생성한 Context를 JdbcTemplate으로 변경해보세요

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -1,39 +1,49 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Created by Cheechyo on 2017. 3. 15..
  */
 public class UserDao {
-    JdbcContext jdbcContext;
-
-    public JdbcContext getJdbcContext() {
-        return jdbcContext;
-    }
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
+    private JdbcTemplate jdbcTemplate;
 
     public User get(Long id) {
         Object[] params = new Object[]{id};
         final String sql = "select * from userinfo where id = ?";
-        return jdbcContext.select(params, sql);
+        return jdbcTemplate.query(sql, params, resultSet -> {
+            User user = null;
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setPassword(resultSet.getString("PASSWORD"));
+            }
+            return user;
+        });
     }
 
     public void add(User user) {
         Object[] params = new Object[]{
-            user.getId()
-            , user.getName()
-            , user.getPassword()
+                user.getId()
+                , user.getName()
+                , user.getPassword()
         };
         final String sql = "INSERT INTO userinfo(ID, NAME, PASSWORD) values (?, ?, ?)";
-        jdbcContext.update(params, sql);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(User user) {
         Object[] params = new Object[]{user.getId()};
         final String sql = "DELETE from userinfo where id = ?";
-        jdbcContext.update(params, sql);
+        jdbcTemplate.update(sql, params);
     }
 
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
