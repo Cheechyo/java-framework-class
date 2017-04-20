f7457efb220564667fb99baa304b07c76c7b01bc
Cheechyo <kwj9211@gmail.com>
2017-04-20 22:00:34 +0900

    4. Connection의 Dependency를 Client로 그리고, Client에서 Factory로 이동하시오(Dpendency Injection)

---
+++ userdao/src/kr/ac/jejunu/DaoFactory.java
@@ -0,0 +1,11 @@
+package kr.ac.jejunu;
+
+/**
+ * Created by Cheechyo on 2017. 4. 20..
+ */
+public class DaoFactory {
+
+    public UserDao getUserDao() {
+        return new UserDao(new DConnectionMaker());
+    }
+}
--- userdao/test/kr/ac/jejunu/UserDaoTest.java
+++ userdao/test/kr/ac/jejunu/UserDaoTest.java
@@ -12,12 +12,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by Cheechyo on 2017. 3. 15..
  */
 public class UserDaoTest {
+    DaoFactory daoFactory;
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1L;
         String name = "Won Ji";
         String password = "Nu Ri";
-        UserDao userDao = new UserDao(new DConnectionMaker());
+        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.get(id);
 
         assertThat(id, is(user.getId()));
@@ -34,38 +35,7 @@ public class UserDaoTest {
         user.setId(id);
         user.setName(name);
         user.setPassword(password);
-        UserDao userDao = new UserDao(new DConnectionMaker());
-        userDao.add(user);
-        user = userDao.get(id);
-
-        assertThat(id, is(user.getId()));
-        assertThat(name, is(user.getName()));
-        assertThat(password, is(user.getPassword()));
-    }
-
-    @Test
-    public void nGet() throws SQLException, ClassNotFoundException {
-        Long id = 1L;
-        String name = "Won Ji";
-        String password = "Nu Ri";
-        UserDao userDao = new UserDao(new NConnectiomMaker());
-        User user = userDao.get(id);
-
-        assertThat(id, is(user.getId()));
-        assertThat(name, is(user.getName()));
-        assertThat(password, is(user.getPassword()));
-    }
-
-    @Test
-    public void nAdd() throws SQLException, ClassNotFoundException {
-        Long id = generateRandomId();
-        String name = "Cheechyo";
-        String password = "cheese";
-        User user = new User();
-        user.setId(id);
-        user.setName(name);
-        user.setPassword(password);
-        UserDao userDao = new UserDao(new NConnectiomMaker());
+        UserDao userDao = daoFactory.getUserDao();
         userDao.add(user);
         user = userDao.get(id);
 
