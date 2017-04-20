238c88dd4bc7c05b14832a44c730b55cc26db918
Cheechyo <kwj9211@gmail.com>
2017-04-20 21:19:53 +0900

    1. 사용자 추가기능을 만들어주세요.

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -7,28 +7,32 @@ import java.sql.*;
  */
 public class UserDao {
     public User get(Long id) throws SQLException, ClassNotFoundException {
-        // User는 mysql에 있음
-        // Class를 로딩해야되겠네
         Class.forName("com.mysql.jdbc.Driver");
-        // 커넥션을 맺기
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "root");
-        // 쿼리를 만들어야겠네
         PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
-        // 쿼리를 실해야겠네
         preparedStatement.execute();
-        // 실행된 결과를 객체에 매핑
         ResultSet resultSet = preparedStatement.getResultSet();
         resultSet.next();
         User user = new User();
         user.setId(resultSet.getLong("ID"));
         user.setName(resultSet.getString("NAME"));
         user.setPassword(resultSet.getString("PASSWORD"));
-        // 자원 해지
         resultSet.close();
         preparedStatement.close();
         connection.close();
-        // 결과를 리턴
         return user;
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "root");
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO userinfo VALUES (?, ?, ?)");
+        preparedStatement.setLong(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+        preparedStatement.execute();
+        preparedStatement.close();
+        connection.close();
     }
 }
