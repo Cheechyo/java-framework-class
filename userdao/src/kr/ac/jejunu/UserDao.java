93587c1551a6c030818ebc5dbcd39cf979d7f62a
Cheechyo <kwj9211@gmail.com>
2017-04-21 13:52:57 +0900

    14. 각자 생성한 Context를 JdbcTemplate으로 변경해보세요

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -1,35 +1,47 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.sql.SQLException;
 
 /**
  * Created by Cheechyo on 2017. 3. 15..
  */
 public class UserDao {
-    public JdbcContext getJdbcContext() {
-        return jdbcContext;
+    JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    JdbcContext jdbcContext;
     public User get(Long id) throws SQLException, ClassNotFoundException {
         Object[] params = {id};
         String sql = "select * from userinfo where id = ?";
-        return jdbcContext.query(params, sql);
+        return jdbcTemplate.query(sql, params, resultSet -> {
+            User user = null;
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setPassword(resultSet.getString("PASSWORD"));
+            }
+            return user;
+        });
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         Object[] params = {user.getId(), user.getName(), user.getPassword()};
         String sql = "INSERT INTO userinfo VALUES(?,?,?)";
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(User user) throws SQLException {
         Object[] params = {user.getId()};
         String sql = "DELETE FROM userinfo WHERE id = ?";
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 }
