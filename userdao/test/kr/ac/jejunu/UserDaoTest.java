d07903027cfc112d0fde2951b3ea5b7bbe4d52ca
Cheechyo <kwj9211@gmail.com>
2017-04-21 11:06:43 +0900

    1. 사용자 추가기능을 만들어주세요.

--- userdao/test/kr/ac/jejunu/UserDaoTest.java
+++ userdao/test/kr/ac/jejunu/UserDaoTest.java
@@ -1,13 +1,12 @@
 package kr.ac.jejunu;
 
-import kr.ac.jejunu.User;
-import kr.ac.jejunu.UserDao;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Cheechyo on 2017. 3. 15..
@@ -23,5 +22,28 @@ public class UserDaoTest {
         assertThat(id, is(user.getId()));
         assertThat(name, is(user.getName()));
         assertThat(password, is(user.getPassword()));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Long id = randomGenerateId();
+        String name = "Cheechyo";
+        String password = "cheese";
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.add(user);
+        user = userDao.get(id);
+        assertThat(id, is(user.getId()));
+        assertThat(name, is(user.getName()));
+        assertThat(password, is(user.getPassword()));
+    }
+
+    private Long randomGenerateId() {
+        Random random = new Random();
+        Long l = new Long(random.nextInt(Integer.MAX_VALUE));
+        return l;
     }
 }
