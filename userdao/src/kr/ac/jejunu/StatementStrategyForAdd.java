a97ed9d98631538a848e2fb2266125ea485c61e6
Cheechyo <kwj9211@gmail.com>
2017-04-21 13:38:26 +0900

    10. Statement코드가 중복이 됩니다. Statement를 추상화해보세요.

---
+++ userdao/src/kr/ac/jejunu/StatementStrategyForAdd.java
@@ -0,0 +1,20 @@
+package kr.ac.jejunu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by Cheechyo on 2017. 4. 21..
+ */
+public class StatementStrategyForAdd implements StatementStrategy {
+    @Override
+    public PreparedStatement makeStatement(Connection connection, Object obj) throws SQLException {
+        User user = (User) obj;
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO userinfo VALUES(?,?,?)");
+        preparedStatement.setLong(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+        return preparedStatement;
+    }
+}
